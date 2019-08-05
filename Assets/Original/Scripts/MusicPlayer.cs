e82cc5f7b7398868e12119ef7b27520df7d8b4c0
yezki8 <yehezkiel.live@gmail.com>
2019-08-05 11:54:28 +0700

    Initial commit, ruangan siap dipakai

---
+++ Assets/Original/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioData;
+
+    public void startMusic()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play();
+        Debug.Log("started");
+    }
+
+    public void pauseMusic()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Pause();
+    }
+
+    public void stopMusic()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Stop();
+    }
+}
