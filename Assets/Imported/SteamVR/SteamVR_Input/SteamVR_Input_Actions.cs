ef27a0629ec5c07c846694b8201ff41f24de7c66
yezki8 <yehezkiel.live@gmail.com>
2019-09-09 17:54:54 +0700

    Progress pointer 50%

--- Assets/Imported/SteamVR/SteamVR_Input/SteamVR_Input_Actions.cs
+++ Assets/Imported/SteamVR/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -51,6 +51,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Pose p_mixedreality_ExternalCamera;
         
+        private static SteamVR_Action_Boolean p_scene1_ButtonAnswer_SelectAnswer;
+        
         public static SteamVR_Action_Boolean default_InteractUI
         {
             get
@@ -187,6 +189,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Boolean scene1_ButtonAnswer_SelectAnswer
+        {
+            get
+            {
+                return SteamVR_Actions.p_scene1_ButtonAnswer_SelectAnswer.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
         private static void InitializeActionArrays()
         {
             Valve.VR.SteamVR_Input.actions = new Valve.VR.SteamVR_Action[] {
@@ -206,7 +216,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.mixedreality_ExternalCamera};
+                    SteamVR_Actions.mixedreality_ExternalCamera,
+                    SteamVR_Actions.scene1_ButtonAnswer_SelectAnswer};
             Valve.VR.SteamVR_Input.actionsIn = new Valve.VR.ISteamVR_Action_In[] {
                     SteamVR_Actions.default_InteractUI,
                     SteamVR_Actions.default_Teleport,
@@ -223,7 +234,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.mixedreality_ExternalCamera};
+                    SteamVR_Actions.mixedreality_ExternalCamera,
+                    SteamVR_Actions.scene1_ButtonAnswer_SelectAnswer};
             Valve.VR.SteamVR_Input.actionsOut = new Valve.VR.ISteamVR_Action_Out[] {
                     SteamVR_Actions.default_Haptic};
             Valve.VR.SteamVR_Input.actionsVibration = new Valve.VR.SteamVR_Action_Vibration[] {
@@ -239,7 +251,8 @@ namespace Valve.VR
                     SteamVR_Actions.default_HeadsetOnHead,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Brake,
-                    SteamVR_Actions.buggy_Reset};
+                    SteamVR_Actions.buggy_Reset,
+                    SteamVR_Actions.scene1_ButtonAnswer_SelectAnswer};
             Valve.VR.SteamVR_Input.actionsSingle = new Valve.VR.SteamVR_Action_Single[] {
                     SteamVR_Actions.default_Squeeze,
                     SteamVR_Actions.buggy_Throttle};
@@ -262,7 +275,8 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Steering,
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
-                    SteamVR_Actions.buggy_Reset};
+                    SteamVR_Actions.buggy_Reset,
+                    SteamVR_Actions.scene1_ButtonAnswer_SelectAnswer};
         }
         
         private static void PreInitActions()
@@ -284,6 +298,7 @@ namespace Valve.VR
             SteamVR_Actions.p_buggy_Brake = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/buggy/in/Brake")));
             SteamVR_Actions.p_buggy_Reset = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/buggy/in/Reset")));
             SteamVR_Actions.p_mixedreality_ExternalCamera = ((SteamVR_Action_Pose)(SteamVR_Action.Create<SteamVR_Action_Pose>("/actions/mixedreality/in/ExternalCamera")));
+            SteamVR_Actions.p_scene1_ButtonAnswer_SelectAnswer = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/Scene1_ButtonAnswer/in/SelectAnswer")));
         }
     }
 }
---
+++ Assets/Original/Scripts/Pointer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class Pointer : MonoBehaviour
+{
+    public static GameObject currentObject;
+    int currentID;
+
+    //Referensi fungsi tombol
+    public SteamVR_Action_Boolean AnswerButton;
+
+    //Referensi tangan
+    public SteamVR_Input_Sources handType;
+
+    //Referensi tombol
+    public Button pilihan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = null;
+        currentID = 0;
+
+        //FUngsi Trigger
+        AnswerButton.AddOnStateDownListener(TriggerDown, handType);
+        AnswerButton.AddOnStateUpListener(TriggerUp, handType);
+    }
+
+    private void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is up)");
+    }
+
+    private void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is down");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+
+            int id = hit.collider.gameObject.GetInstanceID();
+
+            if (currentID != id)
+            {
+                currentID = id;
+                currentObject = hit.collider.gameObject;
+
+                string name = currentObject.tag;
+                if (name == "AnswerButton")
+                {
+                    Debug.Log("Button terdeteksi");
+
+                }
+            }
+        }        
+    }
+}
