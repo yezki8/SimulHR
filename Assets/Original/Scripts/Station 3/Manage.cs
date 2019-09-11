0d12a0e0ee1433cf97849375afe14d57406b1f44
yezki8 <yehezkiel.live@gmail.com>
2019-09-11 18:10:17 +0700

    Progress Station3

---
+++ Assets/Original/Scripts/Station 3/Manage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manage : MonoBehaviour
+{
+    public GameObject Yes, No, YesTrigger, NoTrigger;
+    public Text SoalKanan, SoalKiri;
+    public string[] KumpulanSoalKanan;
+    public string[] KumpulanSoalKiri;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
